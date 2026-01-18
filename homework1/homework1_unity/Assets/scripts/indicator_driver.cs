1405084409d64216f5d5809f204e61a7edade9f4
Joe Maloney <maloney.jg2@gmail.com>
2026-01-18 07:36:19 -0500

    remove unused

--- homework1/homework1_unity/Assets/scripts/indicator_driver.cs
+++ homework1/homework1_unity/Assets/scripts/indicator_driver.cs
@@ -2,16 +2,6 @@ using UnityEngine;
 
 public class indicator_driver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
     //  Sets the indicator to the position given. "pos" is a float, with 0 representing
     //  the left end and 1 representing the right end of the beam
     public void set_pos(float pos)
