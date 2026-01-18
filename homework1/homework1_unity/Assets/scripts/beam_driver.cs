b36fca27da172f94df5b64dba79338ea650bea1b
Joe Maloney <maloney.jg2@gmail.com>
2026-01-17 23:24:27 -0500

    beam is rotating to an angle

--- homework1/homework1_unity/Assets/scripts/beam_driver.cs
+++ homework1/homework1_unity/Assets/scripts/beam_driver.cs
@@ -1,16 +1,44 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class beam_driver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float targetAngleDegrees = 0f;
+
+    private ArticulationBody ab;
+    void Awake()
+    {
+        ab = GetComponent<ArticulationBody>();
+    }
+
     void Start()
     {
 
+        var drive = ab.xDrive;
+        drive.driveType = ArticulationDriveType.Force;
+        drive.damping = 1E5f;
+        drive.stiffness = 1E6f;
+        drive.forceLimit = 1E5f;
+        drive.target = 0;
+        drive.lowerLimit = -89f;
+        drive.upperLimit = 89f;
+        ab.xDrive = drive;
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        set_angle(targetAngleDegrees);
     }
+    // Optional: change angle at runtime
+    public void set_angle(float angleDeg)
+    {
+        var drive = ab.xDrive;
+        drive.target = angleDeg;
+        ab.xDrive = drive;
+    }
+    public float get_angle()
+    {
+        return ab.jointPosition[0] * Mathf.Rad2Deg;
+    }
+
+
 }
