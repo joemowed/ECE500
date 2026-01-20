57d4b2df694193cf89902df76abc941978542d3b
Joe Maloney <maloney.jg2@gmail.com>
2026-01-20 08:32:51 -0500

    beam3 - small model, large angle

--- homework1/homework1_unity/Assets/scripts/agent.cs
+++ homework1/homework1_unity/Assets/scripts/agent.cs
@@ -4,25 +4,29 @@ using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class agent : Agent {
+public class agent : Agent
+{
     public sys sys;
     private int current_episode = 0;
     private float cumulative_reward = 0f;
     private float episode_time = 0f;
     public Transform ball_transform;
-    public override void Initialize() {
+    public override void Initialize()
+    {
         Debug.Log("Agent started");
         current_episode = 0;
         cumulative_reward = 0;
     }
-    public override void OnEpisodeBegin() {
+    public override void OnEpisodeBegin()
+    {
         current_episode++; //track the number of episodes
         cumulative_reward = 0f; //reset reward 
         episode_time = 0;
         sys.random_reset(); //reset w/ random start for the new run
 
     }
-    public override void CollectObservations(VectorSensor sensor) {
+    public override void CollectObservations(VectorSensor sensor)
+    {
 
         sensor.AddObservation(sys.ball_driver.get_pos_vec3());
         sensor.AddObservation(sys.ball_driver.get_velocity_vec3());
@@ -33,28 +37,33 @@ public class agent : Agent {
         sensor.AddObservation(sys.eval.is_in_target());
         sensor.AddObservation(sys.eval.dist_from_target());
     }
-    public override void OnActionReceived(ActionBuffers actions) {
+    public override void OnActionReceived(ActionBuffers actions)
+    {
         set_beam(actions.ContinuousActions);
         episode_time += Time.fixedDeltaTime;
         AddReward(-episode_time * 0.01f); // exponetial slap the agent for going slow
         AddReward(sys.eval.in_target_time * 0.1f);
         //update the reward after the step penalty above
         cumulative_reward = GetCumulativeReward();
-        if (sys.eval.is_stable) {
+        if (sys.eval.is_stable)
+        {
             goal_reached();
         }
-        if (sys.ball_driver.is_falling) {
-            AddReward(-1.0f);
+        if (sys.ball_driver.is_falling)
+        {
+            AddReward(-10.0f);
             cumulative_reward = GetCumulativeReward();
             EndEpisode();
 
         }
     }
-    private void set_beam(ActionSegment<float> action_segment) {
+    private void set_beam(ActionSegment<float> action_segment)
+    {
         float angle = action_segment[0];
         float angle_max = 0.0f;
         angle += 0.5f;
-        if (angle < angle_max || angle > 1 - angle_max) {
+        if (angle < angle_max || angle > 1 - angle_max)
+        {
             AddReward(-1E-2F); //keep the angle in range 0-1
             cumulative_reward = GetCumulativeReward();
         }
@@ -63,8 +72,9 @@ public class agent : Agent {
         sys.beam_driver.set_angle(angle);
 
     }
-    private void goal_reached() {
-        AddReward(1.0f);
+    private void goal_reached()
+    {
+        AddReward(7.0f);
         cumulative_reward = GetCumulativeReward();
         EndEpisode();
     }
