0ea244bac3e279e0173ec430266c8a4b0f16b2bb
joe <maloney.jg2@gmail.com>
2026-03-26 00:16:04 -0400

    working combination

--- python_projects/opencv565/combination.py
+++ python_projects/opencv565/combination.py
@@ -10,10 +10,13 @@ from torch._C import dtype
 
 HEIGHT, WIDTH = 640, 480  # example, replace with your model's input size
 IMG_PATH = "imgs/2026-03-25-090151.jpg"
-sess = ort.InferenceSession(
-    "models/yolo26-night_one.onnx", providers=["CPUExecutionProvider"]
+yolo = ort.InferenceSession(
+    "models/yolo26-night_one.onnx", providers=["CUDAExecutionProvider"]
 )
-ort_helpers.print_interface(sess)
+sess = ort.InferenceSession(
+    "models/metric3d-small.onnx", providers=["CUDAExecutionProvider"]
+)
+ort_helpers.print_interface(yolo)
 # ----------------------------
 # Load image with OpenCV
 # ----------------------------
@@ -37,10 +40,26 @@ while True:
     # ----------------------------
     # Run inference
     # ----------------------------
-    output = sess.run(["output0"], {"images": input_tensor})
+    output = yolo.run(["output0"], {"images": input_tensor})
     # 'output' is your result from session.run()
     # We use [0] to get the first batch
     detections = output[0][0]
+
+    img_norm = img.astype(np.float32) / 255.0
+    mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+    std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+    img_norm = (img_norm - mean) / std
+
+    #    # Convert to CHW and add batch dim
+    input_tensor = np.transpose(img_norm, (2, 0, 1))  # HWC -> CHW
+    input_tensor = np.expand_dims(input_tensor, 0)  # 1,3,H,W
+
+    # ----------------------------
+    # Run inference
+    # ----------------------------
+    outputs = sess.run(["predicted_depth"], {"pixel_values": input_tensor})
+    depth_map = outputs[0]
+    predicted_depth = np.squeeze(depth_map)
 
     for i in range(300):
         # Extract the 6 values for this specific detection
@@ -78,6 +97,12 @@ while True:
     fps_text = f"FPS: {int(fps)}"
     cv2.putText(img, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)  #
 
-    cv2.imshow("Composed", img)
+    # Depth map: normalize to 0-255 for visualization
+    depth_img = cv2.normalize(predicted_depth, None, 0, 255, cv2.NORM_MINMAX).astype(
+        np.uint8
+    )
+
+    cv2.imshow("Input", img)
+    cv2.imshow("Depth", depth_img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
