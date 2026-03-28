24ab32a8daf65d9a3393bd7de6944e2b4c2eff6b
Joe Maloney <maloney.jg2@gmail.com>
2026-03-27 20:36:31 -0400

    sync

--- python_projects/opencv565/launch_ffmpeg.sh
+++ python_projects/opencv565/launch_ffmpeg.sh
@@ -1,4 +1,3 @@
-ffmpeg -f v4l2 -framerate 20 -video_size 640x480 -i /dev/video0 \
--c:v mjpeg -q:v 7 -fflags nobuffer \
--f mjpeg udp://10.137.88.153:5002?pkt_size=1316
-
+ffmpeg -f v4l2 -framerate 30 -video_size 640x480 -i /dev/video0 \
+  -c:v libx264 -preset ultrafast -tune zerolatency \
+  -f mpegts udp://10.137.88.153:5002
