45ef14da7b2f9f44acfd1238304e60cf2d3917f2
Joe Maloney <maloney.jg2@gmail.com>
2026-03-27 20:05:41 -0400

    sync

--- python_projects/opencv565/launch_ffmpeg.sh
+++ python_projects/opencv565/launch_ffmpeg.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 ffmpeg -f v4l2 -i /dev/video0 \
 -vcodec mjpeg \
+-q:v 5 \
 -s 640x480 \
--f mjpeg udp://10.137.88.153:80
+-f mjpeg -fifo_size 500 -an -flush_packets 1 udp://10.137.88.153:5002
 
