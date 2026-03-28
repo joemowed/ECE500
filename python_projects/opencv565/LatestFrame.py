e4539cf81d2d247fd6f5d89860648af5e45aee89
Joe Maloney <maloney.jg2@gmail.com>
2026-03-27 20:51:30 -0400

    sync

--- python_projects/opencv565/LatestFrame.py
+++ python_projects/opencv565/LatestFrame.py
@@ -1,15 +1,21 @@
-import threading
+import os
 import cv2
+import threading
 
 class LatestFrame:
     def __init__(self, src):
+        # Tell FFmpeg to disable internal buffering and force low latency
+        os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp|fflags;nobuffer|flags;low_delay|framedrop;1"
+        
         self.cap = cv2.VideoCapture(src, cv2.CAP_FFMPEG)
+        
+        # Manually set the buffer size to 1 frame if the backend supports it
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        
         self.frame = None
         self.lock = threading.Lock()
         self.running = True
-
         threading.Thread(target=self.update, daemon=True).start()
-
     def update(self):
         while self.running:
             self.cap.grab()
