0d800fd7e61390f3dbf0f8e06a9e412ff5e1f4c1
Joe Maloney <maloney.jg2@gmail.com>
2026-04-06 20:32:21 -0400

    comp

--- python_projects/wifi_disable/aggregate.sh
+++ python_projects/wifi_disable/aggregate.sh
@@ -4,5 +4,6 @@ rm combined.hc22000
 rm passwords.csv
 hcxpcapngtool -o combined.hc22000 --max-essids=10 --all ./**/*.cap
 hcxhashtool -i combined.hc22000 --info=stdout
-hashcat -m 22000 -a 3 -d 1 --potfile-disable --outfile ./passwords.csv  combined.hc22000   ?h?h?h?h?h?h00
+hashcat -m 22000 combined.hc22000 --show --outfile passwords.csv
+hashcat --hwmon-temp-abort 100 -m 22000 -a 3 -d 1  --outfile  "./passwords.csv"  "combined.hc22000"   "?h?h?h?h?h?h00"
 
