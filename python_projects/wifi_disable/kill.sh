b602ef5a4823655f3ede1583e9c501bfb9cf1ffc
Joe Maloney <maloney.jg2@gmail.com>
2026-04-08 15:45:07 -0400

    prog

--- python_projects/wifi_disable/kill.sh
+++ python_projects/wifi_disable/kill.sh
@@ -1,6 +1,20 @@
+#!/bin/bash
 # INTERFACE="wlp0s20f3"
 INTERFACE="wlx289401b9b39f"
 
+FILE="$1"
+
+# Check if file exists
+[[ ! -f "$FILE" ]] && echo "File not found!" && exit 1
+
+# Read the file line by line
+# -r prevents backslash escapes from being interpreted
+while IFS=',' read -r col1 col2 col3; do
+    echo "Processing: $col1"
+    echo "Data: $col2 and $col3"
+    echo "---"
+done < "$FILE"
+exit
 ssid="0999-wombat"
 password="d3b5e800"
 nmcli device disconnect $INTERFACE
