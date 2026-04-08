a0b4ac70b664f57d3c4d66dec1f540759e09010c
joe <maloney.jg2@gmail.com>
2026-04-08 13:29:08 -0400

    prog

--- python_projects/wifi_disable/kill.sh
+++ python_projects/wifi_disable/kill.sh
@@ -1,11 +1,16 @@
-INTERFACE="wlp0s20f3"
-if nmcli --wait 5 device wifi connect "$ssid" password "$password" ifname "$INTERFACE" > /dev/null 2>&1; then
+# INTERFACE="wlp0s20f3"
+INTERFACE="wlx289401b9b39f"
+
+ssid="0999-wombat"
+password="d3b5e800"
+nmcli device disconnect $INTERFACE
+if nmcli --wait 5 device wifi connect "$ssid" password "$password" ifname "$INTERFACE" ; then
     # Manually assign an IP immediately (No waiting for DHCP!)
     # Adjust 192.168.1.50 to an IP likely to be free on your target networks
-    sudo ip addr add 192.168.125.50/24 dev wlan0 2>/dev/null
+    # sudo timeout 1s ip addr add 192.168.125.50/24 dev "$INTERFACE" 
     
-    curl --connect-timeout 1 -s -X 'POST' 'http://192.168.125.1:8888/api/shutdown' 
+    timeout 1.5s curl --connect-timeout 1 -s -X 'POST' 'http://192.168.125.1:8888/api/shutdown' 
     
     # Clean up the IP so the next network doesn't conflict
-    sudo ip addr del 192.168.1.50/24 dev wlan0 2>/dev/null
+    # sudo timeout 1s ip addr del 192.168.1.50/24 dev "$INTERFACE" 
 fi
